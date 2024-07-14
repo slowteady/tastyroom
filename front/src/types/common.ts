6e4b61c4f2d8afbb06fa19462f523fd2b7c7d50e
Lee Yong Min <vbn0213@gmail.com>
2024-07-14 22:32:15 +0900

    feat: add api

---
+++ front/src/types/common.ts
@@ -0,0 +1,28 @@
+import {
+  MutationOptions,
+  QueryKey,
+  UseQueryOptions,
+} from '@tanstack/react-query';
+import {AxiosError} from 'axios';
+
+export type ResponseError = AxiosError<{
+  statusCode: string;
+  message: string;
+  error: string;
+}>;
+
+export type UseMutationCustomOptions<
+  TData = unknown,
+  TVariables = unknown,
+> = Omit<
+  MutationOptions<TData, ResponseError, TVariables, unknown>,
+  'mutationFn'
+>;
+
+export type UseQueryCustomOptions<
+  TQueryFnData = unknown,
+  TData = unknown,
+> = Omit<
+  UseQueryOptions<TQueryFnData, ResponseError, TData, QueryKey>,
+  'queryKey'
+>;
---
+++ front/src/utils/header.ts
@@ -0,0 +1,13 @@
+import {axiosInstance} from '../api/axios';
+
+export const setHeader = (key: string, value: string) => {
+  axiosInstance.defaults.headers.common[key] = value;
+};
+
+export const removeHeader = (key: string) => {
+  if (!axiosInstance.defaults.headers.common[key]) {
+    return;
+  }
+
+  delete axiosInstance.defaults.headers.common[key];
+};
