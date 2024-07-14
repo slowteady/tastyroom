ae32ea008a59e220a0409b8d8acf8141a08229d7
Lee Yong Min <vbn0213@gmail.com>
2024-07-14 22:31:43 +0900

    feat: set boiler plate to  react-query

--- front/App.tsx
+++ front/App.tsx
@@ -1,12 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {QueryClientProvider} from '@tanstack/react-query';
 import React from 'react';
+import {queryClient} from './src/api/queryClient';
 import RootNavigator from './src/navigations/root/RootNavigator';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
 
