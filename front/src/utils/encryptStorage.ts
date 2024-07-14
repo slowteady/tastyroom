68f4e87f1934f458bf5b3c7184b468ed7e038a3b
Lee Yong Min <vbn0213@gmail.com>
2024-07-14 16:23:30 +0900

    feat: add encrypted storage module

---
+++ front/src/utils/encryptStorage.ts
@@ -0,0 +1,19 @@
+import EncryptedStorage from 'react-native-encrypted-storage/lib/typescript/EncryptedStorage';
+
+export const setEncryptStorage = async <T>(key: string, data: T) => {
+  await EncryptedStorage.setItem(key, JSON.stringify(data));
+};
+
+export const getEncryptStorage = async (key: string) => {
+  const storedData = await EncryptedStorage.getItem(key);
+
+  return storedData ? JSON.parse(storedData) : null;
+};
+
+export const removeEncryptStorage = async (key: string) => {
+  const data = await getEncryptStorage(key);
+
+  if (data) {
+    await EncryptedStorage.removeItem(key);
+  }
+};
