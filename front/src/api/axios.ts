62ed976fac1fcef1dc6dfa25de3c93e87c36bb17
Lee Yong Min <vbn0213@gmail.com>
2024-07-21 14:03:46 +0900

    refactor: divide request url

--- front/src/api/axios.ts
+++ front/src/api/axios.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
+import {Platform} from 'react-native';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3030',
+  baseURL:
+    Platform.OS === 'ios' ? 'http://localhost:3030' : 'http://10.0.2.2:3030',
   withCredentials: true,
 });
